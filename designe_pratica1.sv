a080932b2a0602a8b209b07f286a303ff93b30ee
Carlos Vinícius <162610513+CaviSantos@users.noreply.github.com>
2024-12-06 16:20:17 -0300

    Update designe_pratica1.sv

--- designe_pratica1.sv
+++ designe_pratica1.sv
@@ -7,3 +7,16 @@ module half_adder(
   and(c_out,a,b);
   
 endmodule
+
+module full_adder(
+	input a, b, c_in;
+  	output sum, c_out;
+);
+  
+  wire c_out1, sum1, c_out2;
+  
+  half_adder U1 ( sum_1, c_out_1, a, b );
+  half_adder U2 ( sum, c_out_2, sum_1, c_in );
+  or         U3 ( c_out, c_out_1, c_out_2 );
+  
+endmodule
